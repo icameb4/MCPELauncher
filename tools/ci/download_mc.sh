8d18e3c1dc796b4735804cc26747be7713bc1393
XlinksToDo <89912994+MaxSignal@users.noreply.github.com>
2021-09-17 07:29:14 +0900

    l

--- tools/ci/download_mc.sh
+++ tools/ci/download_mc.sh
@@ -4,15 +4,15 @@ rootdir="$PWD"
 cd ..
 mkdir -p mcpe
 cd mcpe
-wget -O mcpe1141.7z "https://cdn.glitch.com/a9068bb8-2329-44b4-8406-ddefb02d1719%2Fmcpe1141.7z?v=1579376927835"
-wget -O mcpe190b3_x86.7z "https://cdn.glitch.com/a9068bb8-2329-44b4-8406-ddefb02d1719%2Fmcpe190b3_x86.7z?v=1579376252305"
-wget -O mcpe180.7z "https://cdn.glitch.com/a9068bb8-2329-44b4-8406-ddefb02d1719%2Fmcpe180.7z?v=1579376255027"
-sha256sum -c "$rootdir/tools/ci/mc_archives_sha256sum.txt"
+wget -O mcpe1141.7z "https://www.dropbox.com/s/573cy58ttklkehy/mcpe1141.7z?dl=1"
+wget -O mcpe190b3_x86.7z "https://www.dropbox.com/s/dxce7fhzbrv05gy/mcpe190b3_x86.7z?dl=1"
+wget -O mcpe180.7z "https://www.dropbox.com/s/1uu4qhdn7tpiwvp/mcpe180.7z?dl=1"
+# sha256sum -c "$rootdir/tools/ci/mc_archives_sha256sum.txt"
 
-7z -p$ASSETS_PASSWORD -aoa x mcpe1141.7z
-7z -p$ASSETS_PASSWORD -aoa x mcpe190b3_x86.7z
-7z -p$ASSETS_PASSWORD -aoa x mcpe180.7z
-sha256sum -c "$rootdir/tools/ci/mc_sha256sum.txt"
+7z -aoa x mcpe1141.7z
+7z -aoa x mcpe190b3_x86.7z
+7z -aoa x mcpe180.7z
+# sha256sum -c "$rootdir/tools/ci/mc_sha256sum.txt"
 
 rm -r 1141 190b3_x86 || true
 7z -o1141 x mcpe1141.apk
